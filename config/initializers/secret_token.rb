36e22d38e24413f9081d6ee78a79bd2b77cf5cc2
frenesim <hlsp999@gmail.com>
2013-11-15 10:50:25 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playtennis::Application.config.secret_key_base = '3aa6a4e46266c2ee73430842cf6702668da18b706cad27498453f4a277fde2895b8a40f6a226fe00ada7c07da0c8ffd999789a61b834129b9f6b0dabe7bac8ed'
